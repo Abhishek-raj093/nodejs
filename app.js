dbe95b9759df9ad816f8768a6a686f41edbde0fc
Abhishek-raj093 <ss0986825@gmail.com>
2025-03-31 07:54:07 +0530

    Created the Different Module in NextJs Like (File System, HTTP, Path, OS)

---
+++ app.js
@@ -0,0 +1,64 @@
+// <--------------------------------- Read File Using File System ---------------------------------------->
+// const fs = require('fs') // Here we are gating the file System
+
+// fs.readFile('example.txt','utf8',(err,data)=>{
+//     if (err) {
+//         console.error(err)
+//         return;
+//     }
+//     console.log(data);
+// })
+
+// <---------------------------------- Write the file using File Syatem ---------------------------------------->
+
+// const fs = require('fs')
+
+// const content = 'Hello, NodeJs'
+
+// fs.writeFile('output.txt', content, (err) => {
+//     if (err) {
+//         console.error(err)
+//         return;
+//     }
+//     console.log('File written successfully')
+// })
+
+// <------------------------------------ HTTP Module --------------------------------------->
+
+// const http = require('http') // Here we will Get the HTTP Module
+
+
+// Here we will Create a simmple Server Using HIIP module
+
+// const server = http.createServer((req,res)=>{
+//     res.statusCode = 200
+//     res.setHeader('Content-Type', 'text/plain')
+//     res.end('Hello, Mufasa')
+// })
+
+// // Here we use the Server Object to Create the Server
+// server.listen(3000,()=>{ //Here we will Provide the 2 Parameters i.e. Port Number and CallBack function
+//     console.log('Server is running on port http://localhost:3000')
+// })
+
+// <---------------------------------------- Path Module --------------------------------------------->
+
+// const path = require('path') // Here we will Get the PATH Module
+
+// // Here we will see one comman example where we will Join and resolve the Path
+// const directory = '/user/local'
+// const fileName = 'example.txt'
+
+// // to get the full Path we will Use the Path Module
+// const fullPath = path.join(directory,fileName) // "path.join" Here this will Help us to Join the Directory and Filename
+// console.log(fullPath) // Here we will get the Full Path of the File
+
+
+// <----------------------------------- OS Module --------------------------------------->
+
+const os = require('os')
+
+console.log('Platform:',os.platform()); // os.platform() it will return the Platform Detail of our System
+console.log('CPU Arcitecture:',os.arch());
+console.log('Total Memory:',os.totalmem());
+console.log('Free Memory:',os.freemem());
